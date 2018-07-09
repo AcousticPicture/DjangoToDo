c52883e0142f085cdd59dbb5e7ab274ceb736b3a
Julia Berauer <julia.berauer@cross4channel.de>
2018-07-09 20:10:16 +0200

    added database

---
+++ todo/todo_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+class Entry(models.Model):
+    text = models.CharField(max_length=2000)
+    due_date = models.DateTimeField()
+    done = models.BooleanField(default=False)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
